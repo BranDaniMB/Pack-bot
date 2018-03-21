1ab6d006b1405e4de6d3333f920b8b2b27c18140
Brandon Daniel <brandanimb@gmail.com>
2018-03-21 16:05:45 -0600

    Update tools.js

--- tools.js
+++ tools.js
@@ -1,8 +1,11 @@
+// Exportamos todos los siguientes métodos
 module.exports = {
+    // Uso de Chat-bot.js - formatea a URL
     toURL: function toURL(username, server) {
         username = username.replace(/\s/g, "_");
         return "http://" + server + ".wikia.com/wiki/User:" + username;
     },
+    // Uso de Chat-bot.js - Crea una fecha en huso UTC
     date: function getDate(today) {
         var dd = today.getUTCDate();
         var mm = today.getUTCMonth() + 1; //today es 0!
@@ -25,6 +28,7 @@ module.exports = {
 
         return hh + ":" + ss + " " + dd + '/' + mm + '/' + yyyy + " UTC";
     },
+    // Añadir a el registro
     addLogs: function addLogs(file, content) {
         // https://gist.github.com/AngelMunoz/ffb0783e16054e210c869f85f241a408
         const fs = require('fs');
@@ -35,6 +39,7 @@ module.exports = {
             }
         });
     },
+    // Borrar todos los registros
     deleteLogs: function deleteLogs(file) {
         // https://gist.github.com/AngelMunoz/ffb0783e16054e210c869f85f241a408
         const fs = require('fs');
