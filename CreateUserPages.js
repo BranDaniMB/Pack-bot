2aa6adbb98f11b4193ad8fce67766314b8b395ae
Brandon Daniel <brandanimb@gmail.com>
2018-03-21 15:57:25 -0600

    Update CreateUserPages.js

--- CreateUserPages.js
+++ CreateUserPages.js
@@ -1,10 +1,24 @@
+/*
+ * Creador BranDaniMB
+ * Este documento se usa para crear las páginas de usuario si el bot se usa en una wiki nueva.
+ * Puedes llamar este metodo importandolo.
+*/
+
+// Resumen de edición
 const summary = "Creando página de usuario";
+
+// Crear instancia de bot
 const bot = require('nodemw'), client = new bot('./config.json');
 
+// Declarar función
 function createUserPages() {
+    //Iniciar sesión con el usuario asignado
     client.logIn(function () {
+        //Obtener el lenguaje de la comunidad
         client.getSiteInfo(['general'], function (err, info) {
+            //Obtener página de usuario que maneja el bot - Cambiar el nombre por el usuario que maneja el bot
             client.getArticle("User:BranDaniMB", function (err, content) {
+                // Verifica la existencia del perfil, sino existe lo crea segun los idiomas y verificaciones siguientes
                 if (content === "" || content === undefined) {
                     if (info.general.lang === "es") {
                         content = "{{raw:w:BranDaniMB|es}}";
@@ -24,7 +38,9 @@ function createUserPages() {
                     }
                 }
             });
+            // Obtener página de usuario del bot - Cambiar el nombre por el nombre de usuario del bot
             client.getArticle("User:Pack-bot", function (err, content) {
+                // Verifica la existencia del perfil, sino existe lo crea segun los idiomas y verificaciones siguientes
                 if (content === "" || content === undefined) {
                     if (info.general.lang === "es") {
                         content = "{{raw:w:Pack-bot|es}}";
@@ -48,4 +64,5 @@ function createUserPages() {
     });
 }
 
+// Exporta el método
 exports.createUserPages = createUserPages();
